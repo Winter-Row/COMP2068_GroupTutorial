ead8aed8bb0e523a523bc4512e62f92cfa5bfc15
MikeBerezhnoy <mberezhn@lakeheadu.ca>
2023-04-11 11:25:25 -0400

    comments on ping

--- commands/ping.js
+++ commands/ping.js
@@ -1,10 +1,17 @@
-const { SlashCommandBuilder } = require('discord.js');
+//the slash command builder is a class that helps you build a slash command without having to manually type out everything.
+const { SlashCommandBuilder } = require("discord.js");
 
+//export
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('ping')
-		.setDescription('Replies with Pong!'),
-	async execute(interaction) {
-		await interaction.reply('Pong!');
-	},
+  //data is a property that is required for a slash command
+  data: new SlashCommandBuilder()
+    //set the name of the command
+    .setName("ping")
+    //set the description of the command
+    .setDescription("Replies with Pong!"),
+  async execute(interaction) {
+    //execute is a function that is called when the command is run
+    //so everything in here will be run when the command is run
+    await interaction.reply("Pong!");
+  },
 };
